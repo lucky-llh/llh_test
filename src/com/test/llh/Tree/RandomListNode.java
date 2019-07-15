f4f1dde797d4f6f01b4ed36c600c0312938e4a08
lucky-llh <1500519114@qq.com>
2019-07-15 20:46:33 +0800

    修改包含随机指针的作用域

--- src/com/test/llh/Tree/RandomListNode.java
+++ src/com/test/llh/Tree/RandomListNode.java
@@ -1,11 +1,11 @@
 package com.test.llh.Tree;
 
 public class RandomListNode {
-    int label;
-    RandomListNode next = null;
-    RandomListNode random = null;
+    public int label;
+   public RandomListNode next = null;
+    public RandomListNode random = null;
 
-    RandomListNode(int label) {
+   public RandomListNode(int label) {
         this.label = label;
     }
 }
